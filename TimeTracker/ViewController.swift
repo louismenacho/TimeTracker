cc91bd4d5ba8c038ad24f84427c3c867f1fc3c86
Louis Menacho <melo3450@gmail.com>
2019-01-16 09:54:03 -0500

    Initial Commit

---
+++ TimeTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeTracker
+//
+//  Created by Louis Menacho on 1/16/19.
+//  Copyright © 2019 Louis Menacho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
